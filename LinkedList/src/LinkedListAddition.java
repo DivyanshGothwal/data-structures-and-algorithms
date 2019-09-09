38bacade3f033ca7c4bfda262e3baaacb539f7c5
Divyansh <shotsdivyansh@gmail.com>
2019-09-10 00:01:09 +0530

    added insert at end

--- LinkedList/src/LinkedListAddition.java
+++ LinkedList/src/LinkedListAddition.java
@@ -9,16 +9,32 @@ public class LinkedListAddition {
 		Scanner sc = new Scanner(System.in);
 		// number of element to be inserted
 		int n = sc.nextInt();
+		System.out.println("Please type 0 for isert at end and 1 for isert at head");
+		int isInsertAtHead = sc.nextInt();
 		LinkedList head = null;
 		for (int i = 0; i < n; i++) {
 			// element to be inserted
 			int element = sc.nextInt();
 			// Always inserting at the end of the list
-			head = insertAtEnd(element, head);
+			head = isInsertAtHead == 1 ? insertAtHead(element, head) : insertAtEnd(element, head);
 		}
 		// print linked list to console.
 		printLinkedList(head);
 		sc.close();
+	}
+
+	private static LinkedList insertAtHead(int element, LinkedList head) {
+		LinkedList newNode = new LinkedList(element);
+		// checking if head is present if not then return one node;
+		if (head == null) {
+			return newNode;
+		}
+		LinkedList temp = head;
+		// update head of the linked list.
+		head = newNode;
+		newNode.next = temp;
+		// return updated head
+		return head;
 	}
 
 	private static void printLinkedList(LinkedList head) {
@@ -30,9 +46,10 @@ public class LinkedListAddition {
 	}
 
 	private static LinkedList insertAtEnd(int element, LinkedList head) {
+		LinkedList newNode = new LinkedList(element);
 		// checking if head is present if not then return one node;
 		if (head == null) {
-			return new LinkedList(element);
+			return newNode;
 		}
 		LinkedList temp = head;
 
@@ -40,7 +57,7 @@ public class LinkedListAddition {
 		while (temp.next != null) {
 			temp = temp.next;
 		}
-		temp.next = new LinkedList(element);
+		temp.next = newNode;
 
 		// always returning head node
 		return head;
