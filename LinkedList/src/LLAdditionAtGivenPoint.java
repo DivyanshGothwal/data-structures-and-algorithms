2625774e417693855877707c53199e54c2b46bed
Divyansh <shotsdivyansh@gmail.com>
2019-09-11 23:17:13 +0530

    added addition in LL at given point

---
+++ LinkedList/src/LLAdditionAtGivenPoint.java
@@ -0,0 +1,66 @@
+import java.util.Scanner;
+
+public class LLAdditionAtGivenPoint {
+
+	public static void main(String args[]) throws Exception {
+		Scanner sc = new Scanner(System.in);
+		LL head = null;
+		int n = sc.nextInt();
+		while (n-- > 0) {
+			int ele = sc.nextInt();
+			int pos = sc.nextInt();
+			head = insertLL(ele, pos, head);
+			printLL(head);
+		}
+		sc.close();
+	}
+
+	private static void printLL(LL head) {
+		LL temp = head;
+		while (temp != null) {
+			System.out.print(temp.data + " ");
+			temp = temp.next;
+		}
+	}
+
+	private static LL insertLL(int ele, int pos, LL head) {
+
+		LL newNode = new LL(ele);
+		int i = 1;
+		LL temp = head;
+		LL prevNode = head;
+		while (temp != null) {
+			if (i == pos) {
+				break;
+			}
+			i++;
+			prevNode = temp;
+			temp = temp.next;
+
+		}
+		if (head == null && pos == 1) {
+			return newNode;
+		} else {
+			if (i < pos) {
+				System.out.println("Can not insert");
+				return head;
+			}
+			newNode.next = temp;
+			if (pos != 1) {
+				prevNode.next = newNode;
+			} else {
+				return newNode;
+			}
+		}
+		return head;
+	}
+}
+
+class LL {
+	int data;
+	LL next;
+
+	LL(int data) {
+		this.data = data;
+	}
+}
