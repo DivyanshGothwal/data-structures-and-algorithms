519c3641ab5d5125a33c82f8bab5622da7231600
divyansh gothwal <shotsdivyansh@gmail.com>
2019-09-15 20:56:06 +0530

    added code for removal of loop in LL

--- LinkedList/src/LLLoopDetection.java
+++ LinkedList/src/LLLoopDetection.java
@@ -10,19 +10,63 @@ public class LLLoopDetection {
 		head.next.next.next.next = new LL(3);
 		head.next.next.next.next.next = new LL(2);
 		head.next.next.next.next.next.next = new LL(12);
-		head.next.next.next.next.next.next =  head.next.next.next;// new LL(12);
+		head.next.next.next.next.next.next.next =  head;// new LL(12);
 
-		System.out.println(isLoopPresent(head));
+		LL loopStartPosition = isLoopPresent(head);
+		if(loopStartPosition == null) {
+			System.out.println("Loop is not present ");
+		}
+		else {
+			LL temp = head;
+			LL temp2 = head;			
+			int lengthOfLoop = calculateLengthOfLoop(loopStartPosition);
+			int i=0;
+			System.out.println("length of loop : "+lengthOfLoop);
+			while(i<lengthOfLoop) {
+				temp = temp.next;
+				i++;
+			}
+			while(temp2!=temp) {
+				temp2 = temp2.next;
+				temp = temp.next;
+			}
+			temp2= head;
+			while(temp2.next != temp) {
+				temp2 = temp2.next;
+			}
+			temp2.next = null;
+			System.out.println("LL after loop removal");
+		}
+		
+		printLL(head);
+	}
+	private static int calculateLengthOfLoop(LL loopStartPosition) {
+		LL temp = loopStartPosition;
+		LL temp2 = loopStartPosition.next;
+		int k=1;
+		while(temp != temp2) {
+			temp2 = temp2.next;
+			k++;
+		}
+		return k;
 	}
 
-	private static boolean isLoopPresent(LL head) {
+	private static void printLL(LL head) {
+		while(head!=null) {
+			System.out.println(head.data);
+			head = head.next;
+		}
+		
+	}
+
+	private static LL isLoopPresent(LL head) {
 		LL slow = head;
 		if (slow == null) {
-			return false;
+			return null;
 		}
 		LL fast = head.next;
 		if (fast == null) {
-			return false;
+			return null;
 		}
 		while (slow != fast && fast != null) {
 			slow = slow.next;
@@ -30,12 +74,12 @@ public class LLLoopDetection {
 			if (fast != null) {
 				fast = fast.next;
 			} else {
-				return false;
+				return null;
 			}
 		}
 		if (slow == fast) {
-			return true;
+			return slow;
 		}
-		return false;
+		return null;
 	}
 }
