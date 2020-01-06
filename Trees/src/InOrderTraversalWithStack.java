bd543c97de6fc53630326c0816088eb0c8523067
divyansh gothwal <shotsdivyansh@gmail.com>
2020-01-06 22:49:50 +0530

    added tree algorithms

---
+++ Trees/src/InOrderTraversalWithStack.java
@@ -0,0 +1,10 @@
+package src;
+
+public class InOrderTraversalWithStack {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ Trees/src/SymmetricTree.java
@@ -0,0 +1,39 @@
+package src;
+
+public class SymmetricTree {
+
+	public static void main(String[] args) {
+		Tree root = new Tree(9);
+		root.left = new Tree(8);
+		root.left.left = new Tree(9);
+		root.left.left.left = new Tree(8);
+		root.left.left.right = new Tree(10);
+		root.left.right = new Tree(7);
+		root.left.right.left = new Tree(6);
+		root.left.right.right = new Tree(8);
+		root.right = new Tree(8);
+		root.right.left = new Tree(7);
+		root.right.right = new Tree(9);
+		root.right.right.left = new Tree(10);
+		isFoldable(root);
+	}
+
+	public static boolean isFoldable(Tree root) {
+		if (root == null) {
+			return true;
+		}
+		return isFoldableUtil(root.left, root.right);
+	}
+
+	public static boolean isFoldableUtil(Tree leftTree, Tree rightTree) {
+		if (leftTree == null && rightTree == null) {
+			return true;
+		}
+		if ((leftTree == null && rightTree != null) || (leftTree != null && rightTree == null)) {
+			return false;
+		}
+		return isFoldableUtil(leftTree.left, rightTree.left) && isFoldableUtil(leftTree.right, rightTree.right)
+				&& leftTree.data == rightTree.data;
+	}
+
+}
