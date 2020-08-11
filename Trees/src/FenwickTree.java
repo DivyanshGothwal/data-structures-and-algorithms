a6412eb44fd10fe9216f6c044da1f388c0796c08
Divyansh Gothwal <shotsdivyansh@gmail.com>
2020-08-11 23:45:31 +0530

    updated fenwik tree and added binary search tree

--- Trees/src/FenwickTree.java
+++ Trees/src/FenwickTree.java
@@ -1,63 +1,49 @@
 package src;
 
 import java.util.*;
+
 //Also Known as Binary Indexed Tree
 public class FenwickTree {
 
 	public static void main(String ar[]) {
-		Scanner sc = new Scanner(System.in);
-		int n = sc.nextInt();
-		int []a = new int [n+1];
-		for(int i=1;i<=n;i++) {
-			a[i] = sc.nextInt();
-		}
-		int q = sc.nextInt();
-		int [] bit = constructFenwickTree(a,a.length);
-		for(int i=0;i<q;i++) {
-			query(sc.nextInt(), bit);
-		}
-		updateBit(a, bit,9,5);
-		sc.close();
+		int a[] = { 1, 3, 5, 7, 9, 11, 0, 7, 9, 4, 6, 8 };
+		int[] bit = constructFenwickTree(a, a.length);
+		query(7, bit);
+		update(bit, 9, 5);
+		Arrays.stream(bit).forEach(e -> System.out.println(e));
 	}
-	public static void query(int end, int []bit) {
-		String s ="";
-		int l = end;
-		while(l>0) {
-			int p = l%2;
-			l=l/2;
-			s=p+s;
+
+	private static int query(int i, int[] bit) {
+		i++;
+		int parent = i;
+		int sum = 0;
+		while (parent != 0) {
+			sum += bit[parent];
+			parent = parent & (parent - 1);
 		}
-		int count=0, ans=0;
-		for(int i=0;i<s.length();i++) {
-			if(s.charAt(i)=='0') {
-				continue;
-			}
-			int pow = 1<<(s.length()-1-i);
-			count+=pow;
-			ans+=bit[count];
-		}
-		System.out.println(ans);
+		System.out.println(sum);
+		return sum;
 	}
-	public static int[] constructFenwickTree(int []a,int n) {
-		int Bit[] = new int[n];
-		
-		for(int i=1;i<n;i++) {
-			updateBit(a, Bit,i,a[i]);
+
+	private static int[] constructFenwickTree(int[] a, int length) {
+		int tree[] = new int[length + 1];
+		for (int i = 0; i < length; i++) {
+			update(tree, i, a[i]);
 		}
-		return Bit;
+		return tree;
 	}
-	
-	public static void updateBit(int[]a, int [] Bit, int i, int ele) {
-		a[i]=ele;
-		int nextEle = i;
-		while(nextEle<Bit.length) {
-			Bit[nextEle]+=ele;
-			nextEle += nextEle & ((~nextEle)+1);
+
+	private static void update(int[] tree, int i, int value) {
+		i++;
+		while (i < tree.length) {
+			tree[i] += value;
+			i = getNextForUpdate(i);
 		}
-		
+	}
+
+	private static int getNextForUpdate(int i) {
+		int towsComplement = (~i) + 1;
+		towsComplement = towsComplement & i;
+		return towsComplement + i;
 	}
 }
-/**
- * 10
-6 4 7 9 4 6 3 8 5 0
- * */
