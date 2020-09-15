ba886d98a85fd95b40d441c7f3858de8265bea9f
Divyansh Gothwal <shotsdivyansh@gmail.com>
2020-09-16 00:39:45 +0530

    updated algorithms for segment tree and lca

--- Trees/src/SegmentsTree.java
+++ Trees/src/SegmentsTree.java
@@ -1,95 +1,94 @@
 package src;
 
-import java.util.*;
-
 public class SegmentsTree {
 	static int[] segmentTree;
 
 	public static void main(String ar[]) {
-		int arr[] =
+		int arr[] = 
 //			{ 2, 5, 3, 8, 7, 6, 9 };
-				{ 1, 3, 5, 7, 9, 11, 0, 7, 9 };
+				{ 1, 3, 5, 7, 9, 11 };
 		int n = arr.length;
-		segmentTree = new int[2 * n - 1];
-		buildTree(arr);
-		Arrays.stream(segmentTree).forEach(e -> {
-			System.out.print(e + " ");
-		});
-		update(arr, 6, 12);
-		System.out.println();
-		System.out.println();
-		Arrays.stream(segmentTree).forEach(e -> {
-			System.out.print(e + " ");
-		});
-		System.out.println();
-		System.out.println(query(arr, 1, 4));
-		System.out.println(query(arr, 3, 4));
-		System.out.println(query(arr, 3, 6));
+
+		segmentTree = new int[calculateSizeOfTree(2 * n - 1)];
+		buildTree(arr, n);
+		System.out.println(arr);
+		System.out.println(query(arr, n, 1, 4));
+		update(arr, 3, 4, n);
+		System.out.println(query(arr, n, 1, 4));
+		update(arr, 3, 10, n);
+		System.out.println(query(arr, n, 1, 4));
 	}
 
-	private static void buildTree(int[] arr) {
-		buildTree(0, arr, 0, arr.length - 1);
+	public static Integer calculateSizeOfTree(int n) {
+		int i = 0;
+		while (n != 0) {
+			i++;
+			n = n >> 1;
+		}
+		return 1 << i;
 	}
 
-	private static void update(int[] arr, int index, int ele) {
-		int diff = ele - arr[index];
-		update(0, arr, index, diff, 0, arr.length - 1);
+	public static void buildTree(int[] arr, int n) {
+		build(0, arr, 0, n - 1);
 	}
 
-	private static int query(int[] arr, int i, int j) {
-		return queryUtil(0, arr, i, j, 0, arr.length - 1);
-	}
-
-	private static int buildTree(int i, int[] arr, int l, int r) {
+	public static Integer build(int i, int ar[], int l, int r) {
 		if (l > r) {
-			return 0;
+			return Integer.MIN_VALUE;
 		}
-		// leaf condition
 		if (l == r) {
-			segmentTree[i] = arr[l];
-			return segmentTree[i];
+			segmentTree[i] = ar[l];
+			return ar[l];
 		}
-
-		int mid = l + ((r - l) / 2);
-		int left = buildTree(2 * i + 1, arr, l, mid);
-		int right = buildTree(2 * i + 2, arr, mid + 1, r);
-		segmentTree[i] = left + right;
+		int mid = l + (r - l) / 2;
+		int left = build(2 * i + 1, ar, l, mid);
+		int right = build(2 * i + 2, ar, mid + 1, r);
+		segmentTree[i] = Math.max(left, right);
 		return segmentTree[i];
 	}
 
-	private static int update(int i, int[] arr, int index, int diff, int l, int r) {
-		// leaf node
-		if (l == r) {
-			// if leaf node is of index passed update leaf nod
-			if (index == l) {
-				segmentTree[i] = arr[l] + diff;
-			}
-			return segmentTree[i];
-		}
-		// partial merge then go left and right
-		if (index >= l && index <= r) {
-			int mid = l + ((r - l) / 2);
-			int left = update(2 * i + 1, arr, index, diff, l, mid);
-			int right = update(2 * i + 2, arr, index, diff, mid + 1, r);
-			segmentTree[i] = left + right;
-			return segmentTree[i];
-		}
-		return segmentTree[i];
+	public static Integer query(int[] ar, int n, int s, int e) {
+		return queryUtil(ar, 0, s, e, 0, n - 1);
 	}
 
-	private static int queryUtil(int i, int[] arr, int startI, int endJ, int l, int r) {
-		// full merge return that segment
-		if (l >= startI && r <= endJ) {
+	public static Integer queryUtil(int ar[], int i, int s, int e, int l, int r) {
+		// no overlap
+		if (r < s || l > e) {
+			return Integer.MIN_VALUE;
+		}
+		// full overlap
+		if (s <= l && r <= e) {
 			return segmentTree[i];
 		}
-		// partial merge then go left and right
-		if (l <= endJ && r >= startI) {
-			int mid = l + (r - l) / 2;
-			int left = queryUtil(2 * i + 1, arr, startI, endJ, l, mid);
-			int right = queryUtil(2 * i + 2, arr, startI, endJ, mid + 1, r);
-			return left + right;
-		}
-		return 0;
+
+		int mid = l + (r - l) / 2;
+		// partial overlap
+		Integer left = queryUtil(ar, 2 * i + 1, s, e, l, mid);
+		Integer right = queryUtil(ar, 2 * i + 2, s, e, mid + 1, r);
+		return Math.max(left, right);
 	}
 
+	public static void update(int[] ar, int i, int value, int n) {
+		ar[i] = value;
+		updateUtil(ar, i, 0, 0, n - 1);
+	}
+
+	public static Integer updateUtil(int[] ar, int idx, int i, int l, int r) {
+		// full overlap
+		if (l == idx && l == r) {
+			segmentTree[i] = ar[idx];
+			return ar[idx];
+		}
+		// no overlap
+		if (idx < l || idx > r) {
+			return segmentTree[i];
+		}
+		// partial overlap
+		int mid = l + (r - l) / 2;
+		int left = updateUtil(ar, idx, 2 * i + 1, l, mid);
+		int right = updateUtil(ar, idx, 2 * i + 2, mid + 1, r);
+		int max = Math.max(left, right);
+		segmentTree[i] = max;
+		return max;
+	}
 }
